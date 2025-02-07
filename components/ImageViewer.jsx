54002900bbd1204a9754fc0f6ac47035db5d69f9
Alejandro de la Rosa <alejandro.ds92@gmail.com>
2025-02-07 01:30:19 -0700

    StickerApp

---
+++ components/ImageViewer.jsx
@@ -0,0 +1,24 @@
+import { StyleSheet } from "react-native";
+import { Image } from "expo-image";
+import PropTypes from "prop-types";
+
+export default function ImageViewer({ imgSource, selectedImage }) {
+  const imageSource = selectedImage ? { uri: selectedImage } : imgSource;
+
+  return <Image source={imageSource} style={styles.image} />;
+  // return <Image source={imgSource} style={styles.image} />;
+}
+
+ImageViewer.propTypes = {
+  imgSource: PropTypes.oneOfType([PropTypes.number, PropTypes.object])
+    .isRequired,
+  selectedImage: PropTypes.string,
+};
+
+const styles = StyleSheet.create({
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+});
