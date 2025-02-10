d22d9582fddb933e49761aeaebf0bb620f6bf217
Alejandro de la Rosa <alejandro.ds92@gmail.com>
2025-02-10 00:47:09 -0700

    AddTab

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -4,54 +4,67 @@ import { Ionicons } from "@expo/vector-icons";
 
 export default function TabLayout() {
   return (
-    <View style={{ flex: 1, backgroundColor: '#ffd33d' }}> 
-      <StatusBar 
-        translucent 
-        backgroundColor="#25292e" 
-        barStyle="light-content" 
+    <View style={{ flex: 1, backgroundColor: "#ffd33d" }}>
+      <StatusBar
+        translucent
+        backgroundColor="#25292e"
+        barStyle="light-content"
       />
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: "#ffd33d",
-        headerStyle: {
-          backgroundColor: "#25292e",
-        },
-        headerShadowVisible: false,
-        headerTintColor: "#fff",
-        tabBarStyle: {
-          backgroundColor: "#25292e",
-        },
-      }}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "Home",
-          tabBarIcon: ({ color, focused }) => (
-            <Ionicons
-              name={focused ? "home-sharp" : "home-outline"}
-              color={color}
-              size={24}
-            />
-          ),
+      <Tabs
+        screenOptions={{
+          tabBarActiveTintColor: "#ffd33d",
+          headerStyle: {
+            backgroundColor: "#25292e",
+          },
+          headerShadowVisible: false,
+          headerTintColor: "#fff",
+          tabBarStyle: {
+            backgroundColor: "#25292e",
+          },
         }}
-      />
-      <Tabs.Screen
-        name="about"
-        options={{
-          title: "About",
-          tabBarIcon: ({ color, focused }) => (
-            <Ionicons
-              name={
-                focused ? "information-circle" : "information-circle-outline"
-              }
-              color={color}
-              size={24}
-            />
-          ),
-        }}
-      />
-    </Tabs>
+      >
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: "Home",
+            tabBarIcon: ({ color, focused }) => (
+              <Ionicons
+                name={focused ? "home-sharp" : "home-outline"}
+                color={color}
+                size={24}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="about"
+          options={{
+            title: "About",
+            tabBarIcon: ({ color, focused }) => (
+              <Ionicons
+                name={
+                  focused ? "information-circle" : "information-circle-outline"
+                }
+                color={color}
+                size={24}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="contact"
+          options={{
+            title: "Contact",
+            tabBarIcon: ({ color, focused }) => (
+              <Ionicons
+                name={focused ? "chatbubbles" : "chatbubbles-outline"}
+                color={color}
+                size={24}
+              />
+            ),
+          }}
+        />
+      </Tabs>
     </View>
   );
 }
